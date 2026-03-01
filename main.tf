03a29c147b38add31f49b4680e6c2415234147b9
loileFilip <filipn7n@gmail.com>
2026-03-01 21:50:49 +0100

    Added main.tf
    Still have to run ./terraform apply, but first change the resource group in a less busy area, due to me only having a student subscription.

---
+++ main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = "azure-lab"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "azure-lab-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet" "lab" {
+  name                 = "azure-lab-subnet"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "lab" {
+  name                = "azure-lab-pip"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "lab" {
+  name                = "azure-lab-nic"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lab.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lab.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "lab" {
+  name                            = "azure-lab-vm"
+  resource_group_name             = azurerm_resource_group.lab.name
+  location                        = azurerm_resource_group.lab.location
+  size                            = "Standard_B1s"
+  admin_username                  = "parsleyfilip"
+  network_interface_ids           = [azurerm_network_interface.lab.id]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "parsleyfilip"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}   
